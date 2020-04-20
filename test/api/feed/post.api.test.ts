0d342db6bc86d104517587fe950b690f0c7e1ddd
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-04-20 19:38:22 -0300

    Add database folder in test directory and place db creation and migrations

--- test/api/feed/post.api.test.ts
+++ test/api/feed/post.api.test.ts
@@ -1,20 +1,8 @@
 import app from "../../../src/server";
 import request from "supertest";
-import { createConnection, getConnection, Connection } from "typeorm";
-import { Post } from "../../../src/entities/feed/post.model";
-import { User } from "../../../src/entities/user/user.model";
+import { getConnection } from "typeorm";
 import { getPostCreateDtoMock } from "../../mocks/feed/post.create.dto.mock";
-
-const createDb = (): Promise<Connection> =>
-  createConnection({
-    type: "sqlite",
-    database: ":memory:",
-    dropSchema: true,
-    entities: [Post, User],
-    synchronize: true,
-    logging: false,
-    migrations: ["test/migrations/**/*.ts"],
-  });
+import { createDb } from "../../database/startup/startup.test.db";
 
 beforeEach(async () => {
   const connection = await createDb();
