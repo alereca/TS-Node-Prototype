d02ac4e6fb16a54543128437de4487606175d416
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-05-02 20:42:26 -0300

    Add error handling middleware
    Using winston logging library

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import winston, { createLogger, transports } from "winston";
+
+const consoleOptions: transports.ConsoleTransportOptions = {
+  level: "info",
+  handleExceptions: true,
+  format: winston.format.simple(),
+};
+
+const logger = createLogger({
+  transports: [new transports.Console(consoleOptions)],
+  exitOnError: false,
+});
+
+export const errorHandlingMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const status = 500;
+  const message = "Something went wrong";
+
+  logger.log({ level: "info", message: err.message });
+
+  res.status(status).json({ message: message });
+};
