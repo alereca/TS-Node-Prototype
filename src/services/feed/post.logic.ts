f989820a9e043ae0aa63ec224cca32e83e47976b
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-05-03 13:25:38 -0300

    Test another way to chain async functions

--- src/services/feed/post.logic.ts
+++ src/services/feed/post.logic.ts
@@ -7,15 +7,19 @@ import { PostShowDto } from "../../entities/feed/output/post.show.dto";
 import { PostCreateDto } from "../../entities/feed/input/post.create.dto";
 import { PostSavedDto } from "../../entities/feed/output/post.saved.dto";
 import { plainToClass } from "class-transformer";
-import { TaskEither } from "fp-ts/lib/TaskEither";
-import { Result } from "neverthrow";
+import { Result, chain, ok } from "neverthrow";
+
+const convertToDto = async (
+  posts: Post[],
+): Promise<Result<PostShowDto[], Error>> =>
+  ok(posts.map((post) => plainToClass(PostShowDto, post)));
 
 export const getPostsLogicFactory = (getFromRepo: getQueryFunc) => (): Promise<
   Result<PostShowDto[], Error>
-> =>
-  getFromRepo(Post, ["user"]).then((result) =>
+> => chain(getFromRepo(Post, ["user"]), convertToDto);
+/* getFromRepo(Post, ["user"]).then((result) =>
     result.map((posts) => posts.map((post) => plainToClass(PostShowDto, post))),
-  );
+  ); */
 
 export const savePostLogicFactory = (save: saveQueryFunc) => (
   value: PostCreateDto,
