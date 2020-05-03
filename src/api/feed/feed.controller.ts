3a44c69241c3825b44ac926024f9438c7ee99195
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-05-03 01:27:24 -0300

    Try fp-ts approach in both service and api layer
    The compiler can't figure the types

--- src/api/feed/feed.controller.ts
+++ src/api/feed/feed.controller.ts
@@ -1,20 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import { Post } from "../../entities/feed/post.model";
-import { getPostsLogic, savePostLogic } from "../../services/feed/post.logic.setup";
+import {
+  getPostsLogic,
+  savePostLogic,
+} from "../../services/feed/post.logic.setup";
 import { PostCreateDto } from "../../entities/feed/input/post.create.dto";
 import { plainToClass } from "class-transformer";
 import { PostShowDto } from "src/entities/feed/output/post.show.dto";
 import { PostSavedDto } from "src/entities/feed/output/post.saved.dto";
+import { TaskEither, fold } from "fp-ts/lib/TaskEither";
 
 export const getPosts = async (
   req: Request,
   res: Response,
   next: NextFunction,
-): Promise<Response<PostShowDto[]>> => {
-  const posts = await getPostsLogic();
+) =>
+  fold<Error, PostShowDto[]>(
+    (err) => next(err),
+    (posts) => res.status(200).json(posts),
+  )(getPostsLogic);
 
-  return res.status(200).json(posts);
-};
+//res.status(200).json(posts);
 
 export const createPost = async (
   req: Request,
--- src/services/feed/post.logic.ts
+++ src/services/feed/post.logic.ts
@@ -1,20 +1,20 @@
-import { getQueryFunc, saveQueryFunc } from "../../utils/common/common.query.interface";
+import {
+  getQueryFunc,
+  saveQueryFunc,
+} from "../../utils/common/common.query.interface";
 import { Post } from "../../entities/feed/post.model";
 import { PostShowDto } from "../../entities/feed/output/post.show.dto";
 import { PostCreateDto } from "../../entities/feed/input/post.create.dto";
 import { PostSavedDto } from "../../entities/feed/output/post.saved.dto";
 import { plainToClass } from "class-transformer";
-import { TaskEither } from "fp-ts/lib/TaskEither";
-import { pipe } from "fp-ts/lib/pipeable";
-import { chain } from "fp-ts/lib/Option";
+import { TaskEither, map } from "fp-ts/lib/TaskEither";
 
-export const getPostsLogicFactory = (getFromRepo: getQueryFunc) => (): TaskEither<
-  Error,
-  PostShowDto[]
-> =>
-  pipe(chain((posts) => posts.map((post) => plainToClass(PostShowDto, post))))(
-    getFromRepo(Post, ["user"]),
-  );
+export const getPostsLogicFactory = (
+  getFromRepo: getQueryFunc,
+) => (): TaskEither<Error, PostShowDto[]> =>
+  map<Post[], PostShowDto[]>((posts) =>
+    posts.map((post) => plainToClass(PostShowDto, post)),
+  )(getFromRepo(Post, ["user"]));
 
 export const savePostLogicFactory = (save: saveQueryFunc) => (
   value: PostCreateDto,
